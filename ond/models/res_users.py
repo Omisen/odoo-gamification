f32031725e39399f1e9f372ff060791a73454925
Omisen <simone.conzo@outlook.it>
2026-07-04 16:22:34 +0200

    [ADD] enhance character management with level and XP tracking in res.users model and view

--- ond/models/res_users.py
+++ ond/models/res_users.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class ResUsers(models.Model):
@@ -17,3 +17,24 @@ class ResUsers(models.Model):
         ],
         string='Class',
     )
+    character_level = fields.Integer(
+        string='Level',
+        default=1,
+        readonly=True,
+    )
+    character_xp = fields.Integer(
+        string='XP',
+        default=0,
+        readonly=True,
+    )
+    character_xp_next_level = fields.Integer(
+        string='XP to next level',
+        compute='_compute_xp_next_level',
+    )
+ 
+    # logica di _compute_xp_next_level per ora è semplicissima - livello × 100. Quindi livello 1 -> 100 XP, livello 5 -> 500 XP
+    # update futuro con logica a curva esponenziale
+    @api.depends('character_level')
+    def _compute_xp_next_level(self):
+        for user in self:
+            user.character_xp_next_level = user.character_level * 100
